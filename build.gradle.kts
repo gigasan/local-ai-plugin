bafcf4a6a922422605d88529e6bbc1514eb537a1
gigasan <9309909+gigasan@users.noreply.github.com>
2026-04-06 07:00:39 +0300

    just got response

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    id("org.jetbrains.intellij.platform") version "2.13.1"
+}
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.11.0")
+    intellijPlatform {
+        rustRover("2025.3.2")
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = "Local AI Plugin"
+        version = "0.1.0"
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+// Убираем ненужные предупреждения при сборке
+tasks.buildSearchableOptions {
+    enabled = false
+}
